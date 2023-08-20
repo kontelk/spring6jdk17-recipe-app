e5d448023a5f8e6a53d22a74c728231006467a13
kontelk <ktelkis@gmail.com>
2023-08-20 17:49:56 +0300

    Initial commit

---
+++ src/main/java/com/kt/spring6jdk17recipeapp/Spring6jdk17RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.kt.spring6jdk17recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6jdk17RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6jdk17RecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kt/spring6jdk17recipeapp/Spring6jdk17RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.spring6jdk17recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6jdk17RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
